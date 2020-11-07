a4676a900264de6007cb9c92384555694c32cc5a
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 12:09:49 -0300

    Criando o schema para contato

--- src/models/contactSchema.js
+++ src/models/contactSchema.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+
+const contactSchema = new Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    auto: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  cellPhone: {
+    type: String,
+    required: true
+  },
+  birthDate: {
+    type: Date,
+    required: true
+  },
+  profilePhoto: {
+    type: String,
+  }
+})
