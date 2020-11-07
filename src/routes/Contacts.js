b3d94d8ed4e0c5c170a6ecceb36a1bfdb4066586
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 14:30:11 -0300

    Criando rota para o POST

--- src/routes/Contacts.js
+++ src/routes/Contacts.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require('../controllers/Contacts')
 
 router.get('/', controller.getAll)
+router.post('/add', controller.addContact)
 
 module.exports = router
