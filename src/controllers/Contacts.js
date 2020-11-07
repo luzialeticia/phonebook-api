efa146c896ea593b68b00b160e46744ed8006be1
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 13:58:03 -0300

    Criando a função getlAll

---
+++ src/controllers/Contacts.js
@@ -0,0 +1,17 @@
+const model = require('../models/contactSchema')
+
+const getAll = (req, res) => {
+  model.find((error, contacts) => {
+    if(error) {
+      return res.status(500).send(error)
+    } else {
+      return res.status(200).send({titulo: "contacts"})
+    }
+  })
+}
+
+
+
+module.exports = {
+  getAll
+}
