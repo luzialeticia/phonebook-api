8c205470cc6ec4471b1b350598aa39a77060e733
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 11:11:06 -0300

    Renomeando o contactsRoutes só para Contacts e chamando no app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const index = require('./routes/index')
+const contacts = require('./routes/Contacts')
 //const db = require('./models/repository')
 
 //db.connect()
 
 app.use(cors())
 app.use(express.json())
-app.use('/')
+app.use('/', index)
+app.use('/contacts', contacts)
 
 module.exports = app
