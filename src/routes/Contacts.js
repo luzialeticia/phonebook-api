384b403de08f35b1873a34421b7136e3ed3e430e
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 15:31:50 -0300

    Criando a rota GET para buscar por id

--- src/routes/Contacts.js
+++ src/routes/Contacts.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const controller = require('../controllers/Contacts')
 
 router.get('/', controller.getAll)
+router.get('/:id', controller.getById)
 router.post('/add', controller.addContact)
 
 module.exports = router
