883536fb7f1f4a612c648eae5b0e399544b03e6e
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 16:08:49 -0300

    Criando função e rota para o DELETE

--- src/controllers/Contacts.js
+++ src/controllers/Contacts.js
@@ -1,7 +1,8 @@
+const { contactsCollection } = require('../models/contactSchema')
 const model = require('../models/contactSchema')
 
 const getAll = (req, res) => {
-  model.contactsCollection.find((error, contacts) => {
+  contactsCollection.find((error, contacts) => {
     if(error) {
       return res.status(500).send(error)
     } else {
@@ -15,7 +16,7 @@ const getAll = (req, res) => {
 
 const getById = (req, res) => {
   const id = req.params.id
-  model.contactsCollection.findById(id, (error, contact) => {
+  contactsCollection.findById(id, (error, contact) => {
     if(error) {
       return res.status(404).send('Contact not found.')
     } else {
@@ -26,7 +27,7 @@ const getById = (req, res) => {
 
 const addContact = (req, res) => {
   const contactBody = req.body
-  const contact = new model.contactsCollection(contactBody) //model.contactsCollection(contactBody)
+  const contact = new contactsCollection(contactBody)
 
   contact.save((error) => {
     if(error) {
@@ -40,8 +41,40 @@ const addContact = (req, res) => {
   })
 }
 
+// const updateContact = (req, res) => {
+//   const id = req.params.id
+//   const contactBody = req.body
+//   const updatedContact = {new: true}
+
+//   model.contactsCollection.findByIdAndUpdate(
+//     id,
+//     {$set:{contactBody}},
+//     updatedContact,
+//     (error, contact) => {
+//       if(error) {
+//         return res.status(404).send('Contact not found.')
+//       } else {
+//         return res.status(200).send(contact)
+//       }
+//     }
+//   )
+// }
+
+const deleteContact = (req, res) => {
+  const id = req.params.id
+  contactsCollection.findByIdAndDelete(id, (error) => {
+    if(error) {
+      return res.status(404).send('Contact not found.')
+    } else {
+      return res.status(200).send('Contact deleted!')
+    }
+  })
+}
+
 module.exports = {
   getAll,
   getById,
-  addContact
+  addContact,
+  //updateContact,
+  deleteContact
 }
--- src/routes/Contacts.js
+++ src/routes/Contacts.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/Contacts')
 router.get('/', controller.getAll)
 router.get('/:id', controller.getById)
 router.post('/add', controller.addContact)
+// router.put('/:id', controller.updateContact)
+router.delete('/delete/:id', controller.deleteContact)
 
 module.exports = router
