e98e592b03a18e496f41bf49d849fbeb2a69d8ca
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 11:32:36 -0300

    Criando a rota GEt e exportando router

--- src/routes/Contacts.js
+++ src/routes/Contacts.js
@@ -2,4 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/Contacts')
 
-router.get('')
+router.get('/', controller.getAll)
+
+module.exports = router
