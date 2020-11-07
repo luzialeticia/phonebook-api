0dba4bedf9dcaa548473139e2c97cfddac9476ae
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 11:55:24 -0300

    Criando a função connect e exportando

--- src/models/repository.js
+++ src/models/repository.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose')
 const DB_URL = 'mongodb://localhost:27017/Reprograma'
 
 const connect = () => {
-  
+  mongoose.connect(DB_URL, { userNewUrlParser: true })
+  const connection = mongoose.connection
+
+  connection.on('error', () => console.error('Error on connection!'))
+  connection.once('open', () => console.log('Database connected!'))
 }
+
+module.exports = connect
