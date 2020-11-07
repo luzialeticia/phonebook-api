4608b4da2b7599fa23a84af650fb49dee108d373
luzialeticia <leticialuzia@tutamail.com>
2020-11-07 14:29:05 -0300

    Criando função para o POST

--- src/controllers/Contacts.js
+++ src/controllers/Contacts.js
@@ -6,15 +6,30 @@ const getAll = (req, res) => {
       return res.status(500).send(error)
     } else {
       return res.status(200).send({
-        mensagem: "Contacts",
+        message: "Sucesso!",
         contacts
       })
     }
   })
 }
 
+const addContact = (req, res) => {
+  const contactBody = req.body
+  const contact = new model.contactsCollection(contactBody)
 
+  contact.save((error) => {
+    if(error) {
+      return res.status(400).send(error)
+    } else {
+      return res.status(202).send({
+        message: "Contato cadastrado com sucesso!",
+        contact
+      })
+    }
+  })
+}
 
 module.exports = {
-  getAll
+  getAll,
+  addContact
 }
